82387dd9ebb0b2f85f3003b0c3cec6d55ce5bc03
โสภณ ไทยอู่ <sopon@office.met.ac.th>
2017-09-18 21:26:08 +0700

    make connection to NetPie; OK

--- app/src/main/java/one/sop/netpie/MainActivity.java
+++ app/src/main/java/one/sop/netpie/MainActivity.java
@@ -1,13 +1,144 @@
 package one.sop.netpie;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import io.netpie.microgear.Microgear;
+import io.netpie.microgear.MicrogearEventListener;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Microgear microgear = new Microgear(this);
+    private String appid = "Metheepittaya";
+    private String key = "J9Kf8JeV6ikpYU5";
+    private String secret = "TAreTCkEdswAtxJgnDrJjjlmn";
+    private String alias = "FromAndroidDevice";
+
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            Bundle bundle = msg.getData();
+            String string = bundle.getString("myKey");
+            TextView myTextView = (TextView) findViewById(R.id.textView);
+            myTextView.append(string + "\n");
+        }   //handleMessage
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        MicrogearCallBack callback = new MicrogearCallBack();
+        microgear.connect(appid, key, secret, alias);
+        microgear.setCallback(callback);
+        microgear.subscribe("Topictest");
+        (new Thread(new Runnable() {
+            int count = 1;
+
+            @Override
+            public void run() {
+                while (!Thread.interrupted())
+                    try {
+                        runOnUiThread(new Runnable() // start actions in UI thread
+                        {
+                            @Override
+                            public void run() {
+                                microgear.publish("Topictest", String.valueOf(count) + ".  Test message");
+                                count++;
+                            }
+                        });
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        // ooops
+                    }
+            }
+        })).start();
+    }   //conCreate
+
+    protected void onDestroy() {
+        super.onDestroy();
+        microgear.disconnect();
+    }   //onDestroy
+
+    protected void onResume() {
+        super.onResume();
+        microgear.bindServiceResume();
+    }   //onDestroy
+
+    class MicrogearCallBack implements MicrogearEventListener {
+        @Override
+        public void onConnect() {
+            Message msg = handler.obtainMessage();
+            Bundle bundle = new Bundle();
+            bundle.putString("myKey", "Now I'm connected with netpie");
+            msg.setData(bundle);
+            handler.sendMessage(msg);
+            Log.i("Connected", "Now I'm connected with netpie");
+        }
+
+        @Override
+        public void onMessage(String topic, String message) {
+            Message msg = handler.obtainMessage();
+            Bundle bundle = new Bundle();
+            bundle.putString("myKey", topic + " : " + message);
+            msg.setData(bundle);
+            handler.sendMessage(msg);
+            Log.i("Message", topic + " : " + message);
+        }
+
+        @Override
+        public void onPresent(String token) {
+            Message msg = handler.obtainMessage();
+            Bundle bundle = new Bundle();
+            bundle.putString("myKey", "New friend Connect :" + token);
+            msg.setData(bundle);
+            handler.sendMessage(msg);
+            Log.i("present", "New friend Connect :" + token);
+        }
+
+        @Override
+        public void onAbsent(String token) {
+            Message msg = handler.obtainMessage();
+            Bundle bundle = new Bundle();
+            bundle.putString("myKey", "Friend lost :" + token);
+            msg.setData(bundle);
+            handler.sendMessage(msg);
+            Log.i("absent", "Friend lost :" + token);
+        }
+
+        @Override
+        public void onDisconnect() {
+            Message msg = handler.obtainMessage();
+            Bundle bundle = new Bundle();
+            bundle.putString("myKey", "Disconnected");
+            msg.setData(bundle);
+            handler.sendMessage(msg);
+            Log.i("disconnect", "Disconnected");
+        }
+
+        @Override
+        public void onError(String error) {
+            Message msg = handler.obtainMessage();
+            Bundle bundle = new Bundle();
+            bundle.putString("myKey", "Exception : " + error);
+            msg.setData(bundle);
+            handler.sendMessage(msg);
+            Log.i("exception", "Exception : " + error);
+        }
+
+        @Override
+        public void onInfo(String info) {
+            Message msg = handler.obtainMessage();
+            Bundle bundle = new Bundle();
+            bundle.putString("myKey", "Exception : " + info);
+            msg.setData(bundle);
+            handler.sendMessage(msg);
+            Log.i("info", "Info : " + info);
+        }
+    }   //Class MicrogearCallback
 }
